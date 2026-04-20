8f49644edd37276a5beebe7387b5f644cc1e53e4
Dan Li <danmuxingli@gmail.com>
2026-04-20 23:32:45 +0000

    6a: synchronize store read and write

--- 6/maelstrom-txn/main.go
+++ 6/maelstrom-txn/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 var (
 	store = make(map[int]int)
+	mu    sync.RWMutex
 )
 
 func main() {
@@ -25,11 +27,15 @@ func main() {
 
 			switch op_type {
 			case "r":
+				mu.RLock()
 				if val, ok := store[op_key]; ok {
 					txn_op[2] = val
 				}
+				mu.RUnlock()
 			case "w":
+				mu.Lock()
 				store[op_key] = int(txn_op[2].(float64))
+				mu.Unlock()
 			}
 		}
 
